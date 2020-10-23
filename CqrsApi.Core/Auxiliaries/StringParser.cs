468f4232fb48859d52e9f968c352082611de50b3
razumovskiy <kolosovp94@gmail.com>
2020-10-23 12:56:01 +0300

    converter tested

--- CqrsApi.Core/Auxiliaries/StringParser.cs
+++ CqrsApi.Core/Auxiliaries/StringParser.cs
@@ -2,5 +2,17 @@
 {
     public static class StringParser
     {
+        public static string Convert(string databaseUrl)
+        {
+            if (!databaseUrl.Contains("postgres"))
+            {
+                return string.Empty;
+            }
+
+            var temp = databaseUrl.Split(@"://");		
+            var parameters = temp[1].Split(':', '@', '/');		
+            var connectionString = $"Server={parameters[2]};Port={parameters[3]};User Id={parameters[0]};Password={parameters[1]};Database={parameters[4]};sslmode=Require;Trust Server Certificate=true;";
+            return connectionString;
+        }
     }
 }
---
+++ CqrsApi.Tests/Tests/ConverterTest.cs
@@ -0,0 +1,28 @@
+﻿using CqrsApi.Auxiliaries;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace CqrsApi.Tests.Tests
+{
+    [TestFixture]
+    public class ConverterTest
+    {
+        [Test]
+        public void Converter_Test()
+        {
+            var str =
+                "postgres://psajiwtsuypith:fd3e5f4a7c04871450bc608bb8451d00f63f654563a788df7ddfb0686679cc17@-203-165-126.compute-1.amazonaws.com:5432/deglced74b0i79";
+
+            var connStr = StringParser.Convert(str);
+            connStr.Should()
+                .Be(
+                    "Server=-203-165-126.compute-1.amazonaws.com;" +
+                    "Port=5432;" +
+                    "User Id=psajiwtsuypith;" +
+                    "Password=fd3e5f4a7c04871450bc608bb8451d00f63f654563a788df7ddfb0686679cc17;" +
+                    "Database=deglced74b0i79;" +
+                    "sslmode=Require;" +
+                    "Trust Server Certificate=true;");
+        }
+    }
+}
