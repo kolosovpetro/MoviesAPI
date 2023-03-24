b5703b4ddb03505453c1c81284685ecf6d950e2f
kolosovpetro <kolosovp94@gmail.com>
2023-03-24 17:18:54 +0100

    more logs

--- CqrsApi.Core/Program.cs
+++ CqrsApi.Core/Program.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
@@ -7,11 +8,36 @@ namespace CqrsApi.Core
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            try
+            {
+                CreateHostBuilder(args).Build().Run();
+                LoggerToFile.PrintToLog("Main successful.");
+            }
+            catch (Exception e)
+            {
+                LoggerToFile.PrintExceptionToFile(e);
+                Console.WriteLine(e);
+                throw;
+            }
         }
 
-        private static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+        private static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            try
+            {
+                var result =  Host.CreateDefaultBuilder(args)
+                    .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+
+                LoggerToFile.PrintToLog("CreateHostBuilder successful.");
+                
+                return result;
+            }
+            catch (Exception e)
+            {
+                LoggerToFile.PrintExceptionToFile(e);
+                Console.WriteLine(e);
+                throw;
+            }
+        }
     }
 }
