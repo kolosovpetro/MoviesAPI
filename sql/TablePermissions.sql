bae0a4b44470be40a1ee21a44ea5db9959b3c110
kolosovpetro <kolosovp94@gmail.com>
2023-06-04 15:18:48 +0200

    add table permissions

---
+++ sql/TablePermissions.sql
@@ -0,0 +1,56 @@
+USE MOVIES_DEV;
+GRANT SELECT ON [dbo].[__EFMigrationsHistory] TO MoviesUser;
+GRANT INSERT ON [dbo].[__EFMigrationsHistory] TO MoviesUser;
+GRANT UPDATE ON [dbo].[__EFMigrationsHistory] TO MoviesUser;
+GRANT ALTER ON [dbo].[__EFMigrationsHistory] TO MoviesUser;
+GRANT CONTROL ON [dbo].[__EFMigrationsHistory] TO MoviesUser;
+GRANT DELETE ON [dbo].[__EFMigrationsHistory] TO MoviesUser;
+
+GRANT SELECT ON [dbo].[Actors] TO MoviesUser;
+GRANT INSERT ON [dbo].[Actors] TO MoviesUser;
+GRANT UPDATE ON [dbo].[Actors] TO MoviesUser;
+GRANT ALTER ON [dbo].[Actors] TO MoviesUser;
+GRANT CONTROL ON [dbo].[Actors] TO MoviesUser;
+GRANT DELETE ON [dbo].[Actors] TO MoviesUser;
+
+GRANT SELECT ON [dbo].[Clients] TO MoviesUser;
+GRANT INSERT ON [dbo].[Clients] TO MoviesUser;
+GRANT UPDATE ON [dbo].[Clients] TO MoviesUser;
+GRANT ALTER ON [dbo].[Clients] TO MoviesUser;
+GRANT CONTROL ON [dbo].[Clients] TO MoviesUser;
+GRANT DELETE ON [dbo].[Clients] TO MoviesUser;
+
+GRANT SELECT ON [dbo].[Copies] TO MoviesUser;
+GRANT INSERT ON [dbo].[Copies] TO MoviesUser;
+GRANT UPDATE ON [dbo].[Copies] TO MoviesUser;
+GRANT ALTER ON [dbo].[Copies] TO MoviesUser;
+GRANT CONTROL ON [dbo].[Copies] TO MoviesUser;
+GRANT DELETE ON [dbo].[Copies] TO MoviesUser;
+
+GRANT SELECT ON [dbo].[Employees] TO MoviesUser;
+GRANT INSERT ON [dbo].[Employees] TO MoviesUser;
+GRANT UPDATE ON [dbo].[Employees] TO MoviesUser;
+GRANT ALTER ON [dbo].[Employees] TO MoviesUser;
+GRANT CONTROL ON [dbo].[Employees] TO MoviesUser;
+GRANT DELETE ON [dbo].[Employees] TO MoviesUser;
+
+GRANT SELECT ON [dbo].[Movies] TO MoviesUser;
+GRANT INSERT ON [dbo].[Movies] TO MoviesUser;
+GRANT UPDATE ON [dbo].[Movies] TO MoviesUser;
+GRANT ALTER ON [dbo].[Movies] TO MoviesUser;
+GRANT CONTROL ON [dbo].[Movies] TO MoviesUser;
+GRANT DELETE ON [dbo].[Movies] TO MoviesUser;
+
+GRANT SELECT ON [dbo].[Rentals] TO MoviesUser;
+GRANT INSERT ON [dbo].[Rentals] TO MoviesUser;
+GRANT UPDATE ON [dbo].[Rentals] TO MoviesUser;
+GRANT ALTER ON [dbo].[Rentals] TO MoviesUser;
+GRANT CONTROL ON [dbo].[Rentals] TO MoviesUser;
+GRANT DELETE ON [dbo].[Rentals] TO MoviesUser;
+
+GRANT SELECT ON [dbo].[Starrings] TO MoviesUser;
+GRANT INSERT ON [dbo].[Starrings] TO MoviesUser;
+GRANT UPDATE ON [dbo].[Starrings] TO MoviesUser;
+GRANT ALTER ON [dbo].[Starrings] TO MoviesUser;
+GRANT CONTROL ON [dbo].[Starrings] TO MoviesUser;
+GRANT DELETE ON [dbo].[Starrings] TO MoviesUser;
