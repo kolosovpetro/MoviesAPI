cfadfcce3210ec017cdfb95a4d840250edc0c697
kolosovpetro <kolosovp94@gmail.com>
2023-08-26 19:05:25 +0200

    in progress

--- scripts/create-aks-cluster.ps1
+++ scripts/create-aks-cluster.ps1
@@ -4,21 +4,33 @@ az login
 # create service principal
 az ad sp create-for-rbac --role contributor --scopes "/subscriptions/f32f6566-8fa0-4198-9c91-a3b8ac69e89a" --name "Movies_AKS_SP" --sdk-auth
 
+# varaibles
+prefix="k01" \
+k8sName="aks-$prefix" \
+location="northeurope" \
+sub="f32f6566-8fa0-4198-9c91-a3b8ac69e89a" \
+rgName="rg-$k8sName" \
+acrName="k8smoviesacr01"
+
+
 # Create resource group
-az group create --location "northeurope" --subscription "f32f6566-8fa0-4198-9c91-a3b8ac69e89a" --name "aks-k8s-rg"
+az group create --location $location --subscription $sub --name $rgName
 
 # Create ACR
-az acr create --resource-group "aks-k8s-rg" --name "aksk8sacr" --sku "Basic"
+az acr create --resource-group $rgName --name $acrName --sku "Basic"
 
 # create AKS cluster
 az aks create --generate-ssh-keys --subscription "f32f6566-8fa0-4198-9c91-a3b8ac69e89a" --node-count 3 --resource-group "aks-k8s-rg" --name "aks-k8s" --tier free
-az aks create --generate-ssh-keys --subscription "f32f6566-8fa0-4198-9c91-a3b8ac69e89a" --node-count 3 --resource-group "cars-island-c01" --name "cars-aks-k8s-c01" --attach-acr "acrcarsislandc01" --tier free
+az aks create --generate-ssh-keys --subscription $sub \
+    --node-count 3 --resource-group $rgName \
+    --name $k8sName --attach-acr $acrName --tier free
 
 # Update exising cluster with ACR
 az aks update -n "aks-k8s" -g "aks-k8s-rg" --attach-acr "aksk8sacr"
 az aks update -n "cars-aks-k8s-d01" -g "cars-island-d01" --attach-acr "acrcarsislandd01"
 
 # connect to cluster
+az aks get-credentials --resource-group $rgName --name $k8sName --subscription $sub
 az aks get-credentials --resource-group "aks-k8s-rg" --name "aks-k8s" --subscription "f32f6566-8fa0-4198-9c91-a3b8ac69e89a"
 az aks get-credentials --resource-group "cars-island-d01" --name "cars-aks-k8s-d01" --subscription "f32f6566-8fa0-4198-9c91-a3b8ac69e89a"
 az aks get-credentials --resource-group "cars-island-c01" --name "cars-aks-k8s-c01" --subscription "f32f6566-8fa0-4198-9c91-a3b8ac69e89a"
@@ -39,8 +51,9 @@ kubectl describe configmap "movies-configmap"
 kubectl describe service "movies-api-service"
 kubectl get pod -o wide
 kubectl apply -f ./api-deployment.azure.yaml
+kubectl apply -f ./api-deployment.azure.yaml --namespace app
 kubectl apply -f ./app-deployment.azure.yaml
-kubectl apply -f ./mssql-deployment/
+kubectl apply -f ./mssql-deployment/ --namespace app
 kubectl apply -f ./api-deployment/
 kubectl get pods
 kubectl delete -f cars-configmap.yaml -n default
--- scripts/ingress-config.sh
+++ scripts/ingress-config.sh
@@ -21,3 +21,29 @@ helm upgrade ingress-nginx nginx-stable/nginx-ingress \
   --set controller.service.annotations."service\.beta\.kubernetes\.io/azure-dns-label-name"=$DNS_LABEL \
   --set controller.service.loadBalancerIP=$STATIC_IP \
   --set controller.service.annotations."service\.beta\.kubernetes\.io/azure-load-balancer-health-probe-request-path"=/healthz
+
+# New try
+
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo update
+
+NAMESPACE=ingress-basic
+helm install ingress-nginx ingress-nginx/ingress-nginx \
+  --create-namespace \
+  --namespace $NAMESPACE \
+  --set controller.service.annotations."service\.beta\.kubernetes\.io/azure-load-balancer-health-probe-request-path"=/healthz
+
+# Add Acr images
+
+REGISTRY_NAME=<REGISTRY_NAME>
+SOURCE_REGISTRY=registry.k8s.io
+CONTROLLER_IMAGE=ingress-nginx/controller
+CONTROLLER_TAG=v1.2.1
+PATCH_IMAGE=ingress-nginx/kube-webhook-certgen
+PATCH_TAG=v1.1.1
+DEFAULTBACKEND_IMAGE=defaultbackend-amd64
+DEFAULTBACKEND_TAG=1.5
+
+az acr import --name $REGISTRY_NAME --source $SOURCE_REGISTRY/$CONTROLLER_IMAGE:$CONTROLLER_TAG --image $CONTROLLER_IMAGE:$CONTROLLER_TAG
+az acr import --name $REGISTRY_NAME --source $SOURCE_REGISTRY/$PATCH_IMAGE:$PATCH_TAG --image $PATCH_IMAGE:$PATCH_TAG
+az acr import --name $REGISTRY_NAME --source $SOURCE_REGISTRY/$DEFAULTBACKEND_IMAGE:$DEFAULTBACKEND_TAG --image $DEFAULTBACKEND_IMAGE:$DEFAULTBACKEND_TAG
