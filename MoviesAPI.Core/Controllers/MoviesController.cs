1c585c28a64aca173c8cda5394d55eb67fa2d680
kolosovpetro <kolosovp94@gmail.com>
2023-09-29 01:00:33 +0200

    add windows logs

--- MoviesAPI.Core/Controllers/MoviesController.cs
+++ MoviesAPI.Core/Controllers/MoviesController.cs
@@ -1,4 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using AutoMapper;
 using MediatR;
@@ -15,10 +18,16 @@ namespace MoviesAPI.Core.Controllers
 {
     [ApiController]
     [Route("api/movies")]
+    [SuppressMessage("Interoperability", "CA1416:Validate platform compatibility")]
     public class MoviesController : Controller, IMovieController
     {
         private readonly IMediator _mediator;
         private readonly IMapper _mapper;
+
+        private const string
+            SectionName = "Application"; // it is reference to the Application event log section, not app name
+
+        private const string SourceName = "EventLogEntryDemo.API.SourceNew";
 
         public MoviesController(IMediator mediator, IMapper mapper)
         {
@@ -33,14 +42,27 @@ namespace MoviesAPI.Core.Controllers
         [SwaggerOperation(Summary = "Returns list of all the movies in database.")]
         public async Task<IActionResult> GetAllMoviesAsync()
         {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                WriteToWindowsEventLog("GetAllMoviesAsync Invoked", EventLogEntryType.Information);
+
             var request = new GetAllMoviesQuery();
             var response = await _mediator.Send(request);
 
             if (response == null)
+            {
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                    WriteToWindowsEventLog("GetAllMoviesAsync Not Found", EventLogEntryType.Error);
                 return NotFound();
+            }
 
             var mappedResponse = _mapper.Map<IList<GetMovieResponse>>(response);
-            return mappedResponse != null ? (IActionResult) Ok(mappedResponse) : NotFound();
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                WriteToWindowsEventLog("GetAllMoviesAsync Success", EventLogEntryType.Information);
+
+            return mappedResponse != null
+                ? Ok(mappedResponse)
+                : NotFound();
         }
 
         /// <summary>
@@ -50,16 +72,33 @@ namespace MoviesAPI.Core.Controllers
         [SwaggerOperation(Summary = "Returns movie by id.")]
         public async Task<IActionResult> GetMovieByIdAsync(int id)
         {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                WriteToWindowsEventLog("GetMovieByIdAsync Invoked", EventLogEntryType.Information);
+            }
+
             if (id < 0)
+            {
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                    WriteToWindowsEventLog($"GetMovieByIdAsync Not found {id}", EventLogEntryType.Error);
                 return BadRequest(new InvalidIdResponse());
+            }
 
             var query = new GetMovieByIdQuery(id);
             var response = await _mediator.Send(query);
 
             if (response == null)
+            {
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                    WriteToWindowsEventLog($"GetMovieByIdAsync Not found {id}", EventLogEntryType.Error);
                 return NotFound(new MovieNotFoundResponse(id));
+            }
 
             var mappedResponse = _mapper.Map<GetMovieResponse>(response);
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                WriteToWindowsEventLog("GetMovieByIdAsync Success", EventLogEntryType.Information);
+
             return Ok(mappedResponse);
         }
 
@@ -70,14 +109,30 @@ namespace MoviesAPI.Core.Controllers
         [SwaggerOperation(Summary = "Adds new movie to database. Returns response.")]
         public async Task<IActionResult> PostMovieAsync(PostMovieCommand command)
         {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                WriteToWindowsEventLog("PostMovieAsync Invoked", EventLogEntryType.Information);
+
             if (command.Year < 1888)
+            {
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                    WriteToWindowsEventLog($"PostMovieAsync Year {command.Year}", EventLogEntryType.Error);
                 return BadRequest(new InvalidYearResponse());
+            }
 
             if (command.Price <= 0)
+            {
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                    WriteToWindowsEventLog($"PostMovieAsync Price {command.Price}", EventLogEntryType.Error);
                 return BadRequest(new InvalidPriceResponse());
+            }
 
             if (command.AgeRestriction <= 0)
+            {
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                    WriteToWindowsEventLog($"PostMovieAsync AgeRestriction {command.AgeRestriction}",
+                        EventLogEntryType.Error);
                 return BadRequest(new InvalidAgeRestrictionResponse());
+            }
 
             var response = await _mediator.Send(command);
             return Ok(response);
@@ -91,15 +146,23 @@ namespace MoviesAPI.Core.Controllers
         public async Task<IActionResult> PatchMovieAsync(PatchMovieCommand command)
         {
             var model = await _mediator.Send(new GetMovieByIdQuery(command.MovieId));
+
             if (model.Title.Contains("F#"))
+            {
                 return BadRequest("F# best language");
+            }
+
             if (command.MovieId < 0)
+            {
                 return BadRequest(new InvalidIdResponse());
+            }
 
             var response = await _mediator.Send(command);
 
             if (response == null)
+            {
                 return NotFound(new MovieNotFoundResponse(command.MovieId));
+            }
 
             return Ok(new PatchMovieSuccessResponse(command.MovieId));
         }
@@ -112,15 +175,44 @@ namespace MoviesAPI.Core.Controllers
         public async Task<IActionResult> DeleteMovieByIdAsync(int id)
         {
             var model = await _mediator.Send(new GetMovieByIdQuery(id));
+
             if (model.Title.Contains("F#"))
+            {
                 return BadRequest("F# best language");
-            
+            }
+
             var command = new DeleteMovieCommand(id);
             var response = await _mediator.Send(command);
+
             if (response == null)
+            {
                 return NotFound(new MovieNotFoundResponse(command.MovieId));
+            }
 
             return Ok(response);
+        }
+
+        private static void WriteToWindowsEventLog(string message, EventLogEntryType type)
+        {
+            var appLog = new EventLog(SectionName);
+            var sourceName = CreateEventSource(SectionName);
+            appLog.Source = sourceName;
+
+            using var eventLog = new EventLog(SectionName);
+            eventLog.Source = sourceName;
+            eventLog.WriteEntry(message, type);
+        }
+
+        private static string CreateEventSource(string currentAppName)
+        {
+            var sourceExists = EventLog.SourceExists(SourceName);
+
+            if (!sourceExists)
+            {
+                EventLog.CreateEventSource(SourceName, currentAppName);
+            }
+
+            return SourceName;
         }
     }
 }
