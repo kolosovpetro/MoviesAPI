082039feae987ad0aa1255f8490e5ae630aa515b
kolosovpetro <kolosovp94@gmail.com>
2023-12-16 10:43:29 +0100

    update script

--- scripts/idempotent_service_run.sh
+++ scripts/idempotent_service_run.sh
@@ -4,10 +4,12 @@ SERVICE_NAME="movies"
 
 if systemctl -q is-active "$SERVICE_NAME"; then
     echo "Service $SERVICE_NAME is running. Restarting..."
-    systemctl restart "$SERVICE_NAME"
+    sudo systemctl restart "$SERVICE_NAME"
+    sudo systemctl status "$SERVICE_NAME"
 else
     echo "Service $SERVICE_NAME doesn't exist. Starting..."
     sudo systemctl daemon-reload
     sudo systemctl start "$SERVICE_NAME"
     sudo systemctl enable "$SERVICE_NAME"
+    sudo systemctl status "$SERVICE_NAME"
 fi
