bbeab5605ba9e5fa882aa8c5b2a614df721fbc27
kolosovpetro <kolosovp94@gmail.com>
2025-11-05 09:17:24 +0100

    configure health checks

---
+++ MoviesAPI.Core/Controllers/HealthController.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace MoviesAPI.Core.Controllers;
+
+[ApiController]
+public class HealthController : Controller
+{
+    private readonly ILogger<HealthController> _logger;
+
+    public HealthController(ILogger<HealthController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    [Route("health/live")]
+    [SwaggerOperation(Summary = "Liveness probe.")]
+    public async Task<IActionResult> Alive()
+    {
+        _logger.LogInformation("Liveness probe executed.");
+
+        var result = await Task.FromResult(Ok("Healthy."));
+
+        return result;
+    }
+
+    [HttpGet]
+    [Route("health/ready")]
+    [SwaggerOperation(Summary = "Readiness probe.")]
+    public async Task<IActionResult> Ready()
+    {
+        _logger.LogInformation("Readiness probe executed.");
+
+        var result = await Task.FromResult(Ok("Ready."));
+
+        return result;
+    }
+}
