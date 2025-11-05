68b9caf8fa2c013f13201e82a1889e1fb9b6445a
kolosovpetro <kolosovp94@gmail.com>
2025-11-05 02:15:27 +0100

    format logs json

--- MoviesAPI.Core/Program.cs
+++ MoviesAPI.Core/Program.cs
@@ -1,6 +1,6 @@
-using System;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace MoviesAPI.Core
 {
@@ -8,36 +8,27 @@ namespace MoviesAPI.Core
     {
         public static void Main(string[] args)
         {
-            try
-            {
-                CreateHostBuilder(args).Build().Run();
-                //LoggerToFile.PrintToLog("Main successful.");
-            }
-            catch (Exception e)
-            {
-                //LoggerToFile.PrintExceptionToFile(e);
-                Console.WriteLine(e);
-                throw;
-            }
+            CreateHostBuilder(args).Build().Run();
         }
 
         private static IHostBuilder CreateHostBuilder(string[] args)
         {
-            try
-            {
-                var result =  Host.CreateDefaultBuilder(args)
-                    .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+            var result = Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.AddJsonConsole(options =>
+                    {
+                        options.UseUtcTimestamp = true;
+                        options.JsonWriterOptions = new System.Text.Json.JsonWriterOptions
+                        {
+                            Indented = true // pretty-print JSON
+                        };
+                    });
+                })
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
 
-                //LoggerToFile.PrintToLog("CreateHostBuilder successful.");
-                
-                return result;
-            }
-            catch (Exception e)
-            {
-                //LoggerToFile.PrintExceptionToFile(e);
-                Console.WriteLine(e);
-                throw;
-            }
+            return result;
         }
     }
 }
