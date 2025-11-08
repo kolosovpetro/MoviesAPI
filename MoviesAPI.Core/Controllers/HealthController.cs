b6e18d7bb1da652e31c4e693d0e147419d50b9be
kolosovpetro <kolosovp94@gmail.com>
2025-11-08 13:51:43 +0100

    remove health checks logs

--- MoviesAPI.Core/Controllers/HealthController.cs
+++ MoviesAPI.Core/Controllers/HealthController.cs
@@ -1,6 +1,5 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using Swashbuckle.AspNetCore.Annotations;
 
 namespace MoviesAPI.Core.Controllers;
@@ -8,20 +7,11 @@ namespace MoviesAPI.Core.Controllers;
 [ApiController]
 public class HealthController : Controller
 {
-    private readonly ILogger<HealthController> _logger;
-
-    public HealthController(ILogger<HealthController> logger)
-    {
-        _logger = logger;
-    }
-
     [HttpGet]
     [Route("health/live")]
     [SwaggerOperation(Summary = "Liveness probe.")]
     public async Task<IActionResult> Alive()
     {
-        _logger.LogInformation("Liveness probe invoke.");
-
         var result = await Task.FromResult(Ok("Healthy."));
 
         return result;
@@ -32,8 +22,6 @@ public class HealthController : Controller
     [SwaggerOperation(Summary = "Readiness probe.")]
     public async Task<IActionResult> Ready()
     {
-        _logger.LogInformation("Readiness probe invoke.");
-
         var result = await Task.FromResult(Ok("Ready."));
 
         return result;
